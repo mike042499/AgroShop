153073f94551df53749a3f51590146d75d38a581
Kasspex <feliphomezparda@gmail.com>
2025-05-07 16:40:10 -0500

    Tarea 8 del Proyecto Final - Vista de Catálogo / Publicaciones (con localStorage)

--- pag_admin/admin.js
+++ pag_admin/admin.js
@@ -1,32 +1,34 @@
-const listaProductos = JSON.parse(localStorage.getItem("KeyLista")) || [];
-let contador = listaProductos?.length;
-console.log("Esta es la lista " + listaProductos);
-console.log("La longitud es ",contador);
+const listaProductos = JSON.parse(localStorage.getItem("KeyLista")) || []; // Se trae los productos del local storage O se inicializa el array
+let contador = listaProductos?.length;// SE cuenta la longitud del json
+// console.log("Esta es la lista " + listaProductos); // se
+// console.log("La longitud es ",contador);
 
 
 
-  document.querySelector(".formulario").addEventListener("submit", function(event) {
+document.querySelector(".formulario").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
 
+    
     const nombre = document.getElementById("nombreProducto").value;
     const precio = document.getElementById("precioProducto").value;
     const imagenInput = document.getElementById("imageUpload");
     const imagen = imagenInput.files[0];
-
- 
+    
+    
     const producto = {
       id: contador++,
       nombre: nombre,
-      precio: precio,
+      precio: "$"+precio,
       imagen: imagen ? imagen.name : null
     };
-
-  
+    
+    
     listaProductos.push(producto);
-
-
-    console.log(JSON.stringify(listaProductos, null, 2));
+    
+  
+    
+    // console.log(JSON.stringify(listaProductos, null, 2));
     localStorage.setItem("KeyLista",JSON.stringify(listaProductos));
 
   });
--- productos/productos.js
+++ productos/productos.js
@@ -1,4 +1,63 @@
+let listaProductos = [];
 
-const db = require('../productos.json')
+function crearCard(nombre, imagen, precio){
+const contenedor = document.getElementById('contenedor-cards');
+    contenedor.innerHTML += ` <div class="card">
+                <div class="card-image">
+                    <img src="${imagen}" alt="Imagen de la card">
+                    </div>
+                    <div class="card-content">
+                        <h2>${precio}</h2>
+                        <p>${nombre}</p>
+                    </div>
+                </div>`
+}
 
-console.log(db);
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    
+fetch('../productos.json')
+.then(response => response.json())
+.then(data => {console.log(data)
+    listaProductos = data.productos;
+    console.log("Esta es la lista", listaProductos);
+    let Lista = listaProductos;
+    
+    Lista.forEach(element => {
+        crearCard(element.nombre, element.imagen, element.precio)
+    
+});
+})
+.catch(error => console.error('Error:', error));
+
+
+let local = JSON.parse(localStorage.getItem("KeyLista"))
+console.log(local);
+
+local.forEach(element => {
+    crearCard(element.nombre, "../img/arroz-removebg-preview.png", element.precio)
+});
+
+});
+
+// fetch('../productos.json')
+//     .then(response => response.json();
+//     })
+//     .then(data => {
+//         // Si el JSON contiene un objeto con una propiedad "productos"
+//         const productos = Array.isArray(data) ? data : data.productos;
+//         if (!Array.isArray(productos)) {
+//             throw new Error('El JSON no contiene un arreglo válido.');
+//         }
+//         productos.forEach(producto => {
+//             console.log(`ID: ${producto.id}, Producto: ${JSON.stringify(producto)}`);
+//         });
+//     })
+//     .catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+
+
+
