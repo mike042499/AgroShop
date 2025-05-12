321c538724a9cea27cab7f36a80599f21fd36ae4
Michaell Gonzalez Garcia <miagonzalezg@udistrital.edu.co>
2025-05-12 15:40:56 -0500

    tarea 9 Developer

---
+++ pagina_registro/form_reg.js
@@ -0,0 +1,128 @@
+const listaUsuarios = JSON.parse(localStorage.getItem("KeyUsuarios")) || [];
+let contador = listaUsuarios.length;
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("registroForm").addEventListener("submit", tomar_datos);
+});
+function tomar_datos(event){
+   // Evita que el formulario se envíe de forma predeterminada
+    event.preventDefault();
+    limpiarErrores();
+    const nombre = document.getElementById("nombre").value;
+    const email= document.getElementById("email").value;
+    const celular= document.getElementById("celular").value;
+    const localidad= document.getElementById("ubicacion").value;
+    const direccion= document.getElementById("direccion").value;
+    const contraseña=document.getElementById("password").value;
+    const validacion=document.getElementById("conf_password").value;
+    const aprobado=validarForm(nombre,email,celular,direccion,contraseña,validacion);
+    if(aprobado){
+       const usuario = {
+            id: contador++,
+            nombre: nombre,
+            email: email,
+            celular: celular,
+            ciudad: localidad,
+            direccion: direccion,
+            contraseña: contraseña
+        };
+        listaUsuarios.push(usuario);
+    // console.log(JSON.stringify(listaProductos, null, 2));
+        localStorage.setItem("KeyUsuarios",JSON.stringify(listaUsuarios));
+        alert("Registro Exitoso");
+        limpiarFormulario();
+    }
+}
+
+
+function validarForm(nombre,email,celular,direccion,contraseña,validacion){
+    let bandera=false;
+    const nombre_validar=validarNombre(nombre);
+    const email_validar=validarEmail(email);
+    const celular_validar=validarCelular(celular);
+    // const ciudad_validar=validarCiudad(ciudad);
+    const direccion_validar=validarDireccion(direccion);
+    const contraseña_validar=validarContraseña(contraseña);
+    const comparacion_contraseña=validarComparacion(contraseña,validacion);
+    if(nombre_validar && email_validar && celular_validar && direccion_validar && contraseña_validar&&comparacion_contraseña){
+        bandera=true;
+    }
+    return bandera;
+}
+
+function validarEmail(email) {
+    // Expresión regular para validar el formato del correo electrónico
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!regex.test(email)){
+        document.getElementById("errorCorreo").textContent="El correo no es válido";
+    }
+    return regex.test(email);
+}
+function validarCelular(celular) {
+    // Expresión regular para validar el formato del número de celular
+    const regex = /^\d{10}$/; // Cambia esto según el formato que necesites
+    if(!regex.test(celular)){
+        document.getElementById("errorTelefono").textContent="El numero no es válido";
+    }
+    return regex.test(celular);
+}
+function validarNombre(nombre){
+    // Expresión regular para validar el formato del nombre
+    const regex = /^[a-zA-Z\s]+$/; // Cambia esto según el formato que necesites
+    if(!regex.test(nombre)){
+        document.getElementById("errorNombre").textContent="El nombre no es válido";
+    }
+    return regex.test(nombre);
+}
+// function validarCiudad(ciudad){
+//     // Expresión regular para validar el formato de la ciudad
+//     const regex = /^[a-zA-Z\s]+$/; // Cambia esto según el formato que necesites
+//     if(!regex.test(ciudad)){
+//         document.getElementById("errorCiudad").textContent="La ciudad no es valida";
+//     }
+//     return regex.test(ciudad);
+// }
+function validarDireccion(direccion){
+    // Expresión regular para validar el formato de la dirección
+    const regex = /^[a-zA-Z0-9\s,.\-#]+$/; // Cambia esto según el formato que necesites
+    if(!regex.test(direccion)){
+        document.getElementById("errorDireccion").textContent="La dirección no es válida";
+    }
+    return regex.test(direccion);
+}
+function validarContraseña(contraseña){
+    // Expresión regular para validar el formato de la contraseña
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/; // Cambia esto según el formato que necesites
+    if(!regex.test(contraseña)){
+        document.getElementById("errorPassword").textContent="La contraseña no es válida";
+    }
+    return regex.test(contraseña);
+}
+
+function limpiarErrores(){
+    document.getElementById("errorNombre").textContent="";
+    document.getElementById("errorCorreo").textContent="";
+    document.getElementById("errorTelefono").textContent="";
+    document.getElementById("errorCiudad").textContent="";
+    document.getElementById("errorDireccion").textContent="";
+    document.getElementById("errorPassword").textContent="";
+    document.getElementById("errorConfPassword").textContent="";
+}
+
+function validarComparacion(contraseña,validacion){
+    if(contraseña!=validacion){
+        document.getElementById("errorConfPassword").textContent="Las contraseñas no coinciden";
+        return false;
+    }
+    return true;
+}
+
+function limpiarFormulario(){
+    document.getElementById("nombre").value="";
+    document.getElementById("email").value="";
+    document.getElementById("celular").value="";
+    document.getElementById("ubicacion").value="";
+    document.getElementById("direccion").value="";
+    document.getElementById("password").value="";
+    document.getElementById("conf_password").value="";
+}
